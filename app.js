b5b91cfc413eca0efb01a56cfc683228f1b43b75
weshare237 <fopaduclair2000@gmail.com>
2022-07-01 21:15:04 +0100

    Add task, get single task, get all tasks and delete task

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-console.log('Task Manager App')
+require('dotenv').config()
+const connectDB = require('./db/connect')
+const { response } = require('express')
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 5000
+const tasksRouter = require('./routes/tasks')
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.use('/api/v1/tasks', tasksRouter)
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI)
+    app.listen(port, console.log(`Server listening on port ${port}`))
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+start()
